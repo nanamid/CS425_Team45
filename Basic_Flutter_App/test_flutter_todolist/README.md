1fe7a0bc5c4c921ce9ad770c15cd745ad729292b
wpunr <williampeterson@nevada.unr.edu>
2023-11-24 14:34:21 -0800

    Initial 'Empty Application' Template

---
+++ Basic_Flutter_App/test_flutter_todolist/README.md
@@ -0,0 +1,3 @@
+# test_flutter_todolist
+
+A new Flutter project.
---
+++ Basic_Flutter_App/test_flutter_todolist/android/app/src/main/kotlin/com/example/test_flutter_todolist/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.test_flutter_todolist
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
