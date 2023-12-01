04ef05e1531d566efdc0958d8b38d45db07b6ceb
wpunr <williampeterson@nevada.unr.edu>
2023-11-30 17:18:18 -0800

    Hive TypeAdapter object storage

--- Basic_Flutter_App/flutter_walkthrough_app/lib/data/tasklist_classes.dart
+++ Basic_Flutter_App/flutter_walkthrough_app/lib/data/tasklist_classes.dart
@@ -1,7 +1,14 @@
 // TODO annotate these so hive can store the fields
+import 'package:hive/hive.dart';
 
+part 'tasklist_classes.g.dart';
+
+@HiveType(typeId: 0)
 class TaskList {
+  @HiveField(0)
   final int listID;
+
+  @HiveField(1)
   List<Task> list = <Task>[];
 
   TaskList({
@@ -9,19 +16,36 @@ class TaskList {
   });
 }
 
+@HiveType(typeId: 1)
 enum TaskStatus {
+  @HiveField(0)
   TODO,
+
+  @HiveField(1)
   DONE,
 }
 
+@HiveType(typeId: 2)
 class Task {
+  @HiveField(0)
   final int taskID;
+
+  @HiveField(1)
   String? taskName;
+
+  @HiveField(2)
   TaskStatus? taskStatus;
+
+  @HiveField(3)
   String? taskLabel;
-  // timestamp deadline;
+
+  @HiveField(4)
   String? description;
+
+  // timestamp deadline;
+  // @HiveField(5)
   // also need time clock entries and total
+  // @HiveField 6, 7, 8
 
   Task({
     required this.taskID,
@@ -31,3 +55,5 @@ class Task {
     // TODO timeclock stuff
   });
 }
+
+// TODO timestamp class: list of timestamp pairs
---
+++ Basic_Flutter_App/flutter_walkthrough_app/lib/data/tasklist_classes.g.dart
@@ -0,0 +1,127 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'tasklist_classes.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class TaskListAdapter extends TypeAdapter<TaskList> {
+  @override
+  final int typeId = 0;
+
+  @override
+  TaskList read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return TaskList(
+      listID: fields[0] as int,
+    )..list = (fields[1] as List).cast<Task>();
+  }
+
+  @override
+  void write(BinaryWriter writer, TaskList obj) {
+    writer
+      ..writeByte(2)
+      ..writeByte(0)
+      ..write(obj.listID)
+      ..writeByte(1)
+      ..write(obj.list);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is TaskListAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
+
+class TaskAdapter extends TypeAdapter<Task> {
+  @override
+  final int typeId = 2;
+
+  @override
+  Task read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Task(
+      taskID: fields[0] as int,
+      taskName: fields[1] as String?,
+      taskLabel: fields[3] as String?,
+      description: fields[4] as String?,
+    )..taskStatus = fields[2] as TaskStatus?;
+  }
+
+  @override
+  void write(BinaryWriter writer, Task obj) {
+    writer
+      ..writeByte(5)
+      ..writeByte(0)
+      ..write(obj.taskID)
+      ..writeByte(1)
+      ..write(obj.taskName)
+      ..writeByte(2)
+      ..write(obj.taskStatus)
+      ..writeByte(3)
+      ..write(obj.taskLabel)
+      ..writeByte(4)
+      ..write(obj.description);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is TaskAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
+
+class TaskStatusAdapter extends TypeAdapter<TaskStatus> {
+  @override
+  final int typeId = 1;
+
+  @override
+  TaskStatus read(BinaryReader reader) {
+    switch (reader.readByte()) {
+      case 0:
+        return TaskStatus.TODO;
+      case 1:
+        return TaskStatus.DONE;
+      default:
+        return TaskStatus.TODO;
+    }
+  }
+
+  @override
+  void write(BinaryWriter writer, TaskStatus obj) {
+    switch (obj) {
+      case TaskStatus.TODO:
+        writer.writeByte(0);
+        break;
+      case TaskStatus.DONE:
+        writer.writeByte(1);
+        break;
+    }
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is TaskStatusAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
