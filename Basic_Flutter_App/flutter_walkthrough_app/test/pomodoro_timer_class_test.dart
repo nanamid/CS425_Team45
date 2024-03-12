924edd9959d277899919062e4ce84a57b61e8629
wpunr <williampeterson@nevada.unr.edu>
2024-03-12 11:28:36 -0700

    Make pomodoro timer tests work again

--- Basic_Flutter_App/flutter_walkthrough_app/test/pomodoro_timer_class_test.dart
+++ Basic_Flutter_App/flutter_walkthrough_app/test/pomodoro_timer_class_test.dart
@@ -1,11 +1,12 @@
 import 'package:test_app/data/pomodoro_timer_class.dart';
-import 'package:test/test.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
 
 void main()
 {
+  TestWidgetsFlutterBinding.ensureInitialized();
   group('Test pomodoro_timer_class', () {
-    final PomodoroTimer pomo = PomodoroTimer(userTimerCallback: (){});
+    final PomodoroTimer pomo = PomodoroTimer(userTimerCallback: (){}, duration: Duration.zero);
     test('Constructor', () {
       expect(pomo, isA<PomodoroTimer>());
       expect(pomo.timerStartTime, isNull);
@@ -16,26 +17,25 @@ void main()
     });
     test('startTimer', () {
       final Duration dura = Duration(seconds: 1);
-      final PomodoroTimer pomo = PomodoroTimer();
+      final PomodoroTimer pomo = PomodoroTimer(duration: dura);
       expect(pomo.timerIsRunning, false);
-      pomo.startTimer(dura);
+      pomo.startTimer();
       expect(pomo.timerIsRunning, true);
       expect(pomo.timerStartTime, isNotNull);
       expect(pomo.timerEndTime, isNotNull);
       final difference = pomo.timerEndTime!.difference(pomo.timerStartTime!);
       expect(difference.inSeconds, closeTo(1, 0.5));
-    }, skip: 'Requires android to be running, no longer suitable for a unit test');
+    });
     test('Did timer callback fire?', () {
       
-    }, skip: 'Requires an async future to check this');
+    });
     test('stopTimer', () {
       final Duration dura = Duration(seconds: 1);
-      final PomodoroTimer pomo = PomodoroTimer();
-      pomo.startTimer(dura);
-
+      final PomodoroTimer pomo = PomodoroTimer(duration: dura);
+      pomo.startTimer();
       pomo.stopTimer();
       expect(pomo.timerIsRunning, false);
       expect(pomo.remaningTime.inMicroseconds, greaterThan(0));
-    }, skip: 'Requires android to be running, no longer suitable for a unit test');
-  }, skip: 'Requires android to be running, no longer suitable for a unit test');
+    });
+  },);
 }
