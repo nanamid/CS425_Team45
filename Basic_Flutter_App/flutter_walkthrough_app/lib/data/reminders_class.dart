1b43614a1bf396c74874307adab1a215da23338e
wpunr <williampeterson@nevada.unr.edu>
2024-03-19 14:31:10 -0700

    More reminders class work and timers

--- Basic_Flutter_App/flutter_walkthrough_app/lib/data/reminders_class.dart
+++ Basic_Flutter_App/flutter_walkthrough_app/lib/data/reminders_class.dart
@@ -2,6 +2,7 @@ import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'dart:isolate';
 import 'package:test_app/data/tasklist_classes.dart';
+import 'dart:async';
 
 /* 
 needs to set/unset ongoing notification/alarm
@@ -13,6 +14,9 @@ purpose is to abstract away alarm id management
 class ReminderManager {
   final List<int> _alarmIDs = [];
   List<int> get alarmIDs => List.unmodifiable(_alarmIDs);
+
+  final List<Reminder> _allReminders = [];
+  List<Reminder> get allReminders => List.unmodifiable(_allReminders);
 
   final List<Reminder> _remindersWithPersistentNotifications = [];
   List<Reminder> get remindersWithPersistentNotifications =>
@@ -26,27 +30,65 @@ class ReminderManager {
   List<Reminder> get remindersWithAlarms =>
       List.unmodifiable(_remindersWithAlarms);
 
-  final Map<Task, Reminder> _taskReminderMap = Map<Task, Reminder>();
-  Map<Task, Reminder> get taskReminderMap => Map.unmodifiable(_taskReminderMap);
+  // it is important that it is <Reminder, Task> not <Task, Reminder>
+  // Keys map to only one value, but one value can be associated with many keys
+  // Each Reminder can only be associated with one task, while a Task can have multiple reminders
+  final Map<Reminder, Task> _taskReminderMap = Map<Reminder, Task>();
+  Map<Reminder, Task> get taskReminderMap => Map.unmodifiable(_taskReminderMap);
 
   Reminder createReminderForTimer(Duration dura,
       {bool persistentNotification = false,
       bool timerEndNotification = true,
       bool alarm = true}) {
-    Reminder reminder = Reminder(DateTime.now(), DateTime.now().add(dura));
+    Reminder reminder = Reminder(DateTime.now(), DateTime.now().add(dura), (){});
 
     if (persistentNotification) {}
     if (timerEndNotification) {}
     if (alarm) {}
 
+    _allReminders.add(reminder);
     return reminder;
   }
 
   Reminder createReminderForDeadline(DateTime deadline) {
-    return Reminder(DateTime.now(), DateTime.now());
+    Reminder reminder = Reminder(DateTime.now(), deadline, (){});
+    _allReminders.add(reminder);
+    return reminder;
   }
 
   void cancelReminder(Reminder reminder) {}
+
+  void registerTaskWithReminder(Reminder reminder, Task task) {
+    if (this._taskReminderMap[reminder] != null) {
+      print('reminder-task pair already mapped');
+      return;
+    }
+    this._taskReminderMap[reminder] = task;
+  }
+
+  void unregisterReminder(Reminder reminder) {
+    if (this._taskReminderMap[reminder] == null) {
+      print('reminder-task pair does not exist');
+      return;
+    }
+    this._taskReminderMap.remove(reminder);
+  }
+
+  void unregisterAllRemindersOfTask(Task task) {
+    if (!this._taskReminderMap.values.contains(task)) {
+      print('task has no reminders registered');
+      return;
+    }
+    this._taskReminderMap.removeWhere((key, value) => value == task);
+  }
+
+  List<Reminder> getAllRemindersOfTask(Task task) {
+    List<Reminder> list = [];
+    this._taskReminderMap.forEach((key, value) {
+      list.add(key);
+    });
+    return list;
+  }
 
   ReminderManager();
 }
@@ -59,7 +101,24 @@ class Reminder {
   final DateTime _timerEndTime;
   DateTime get timerEndTime => _timerEndTime;
 
-  Reminder(DateTime startTime, DateTime endTime)
+  late final Timer timer;
+
+  Duration get totalDuration => _timerEndTime.difference(_timerStartTime);
+  Duration get remainingDuration => _timerEndTime.difference(DateTime.now());
+
+  // think of this as a destructor
+  // rather than relying on a finalizer, just explicitly kill the reminder
+  void killReminder()
+  {
+    this.timer.cancel();
+  }
+
+  Reminder(DateTime startTime, DateTime endTime, void Function() callback)
       : this._timerStartTime = startTime,
-        this._timerEndTime = endTime;
+        this._timerEndTime = endTime {
+    timer = Timer(this.totalDuration, () {
+      print("Reminder callback fired");
+      callback();
+    });
+  }
 }
--- Basic_Flutter_App/flutter_walkthrough_app/test/reminders_class_test.dart
+++ Basic_Flutter_App/flutter_walkthrough_app/test/reminders_class_test.dart
@@ -9,16 +9,22 @@ void main() {
     test('Constructor', () {
       final ReminderManager rm = ReminderManager();
       expect(rm.alarmIDs, isEmpty);
+      expect(rm.allReminders, isEmpty);
       expect(rm.remindersWithPersistentNotifications, isEmpty);
       expect(rm.remindersWithEndNotifications, isEmpty);
       expect(rm.remindersWithAlarms, isEmpty);
       expect(rm.taskReminderMap, isEmpty);
     });
     test('list getters are unmodifiable', () {
-      final Reminder reminder = Reminder(DateTime.now(), DateTime.now());
+      final Reminder reminder = Reminder(DateTime.now(), DateTime.now(), (){});
       final ReminderManager rm = ReminderManager();
       try {
         rm.alarmIDs.add(5);
+      } catch (e) {
+        expect(e, isUnsupportedError);
+      }
+      try {
+        rm.allReminders.add(reminder);
       } catch (e) {
         expect(e, isUnsupportedError);
       }
@@ -38,14 +44,14 @@ void main() {
         expect(e, isUnsupportedError);
       }
       try {
-        rm.taskReminderMap[Task(taskName: "foo")] = reminder;
+        rm.taskReminderMap[reminder] = Task(taskName: "foo");
       } catch (e) {
         expect(e, isUnsupportedError);
       }
     });
     test('reminders inside getter lists should be immutable', () {
       // needs timers to get added
-    });
+    }, skip: "needs timers to get added");
     test('reminder for timer', () {
       final ReminderManager rm = ReminderManager();
       final r = rm.createReminderForTimer(Duration(seconds: 1));
@@ -53,16 +59,137 @@ void main() {
           DateTime.now().difference(r.timerStartTime).inSeconds, closeTo(0, 1));
       expect(
           DateTime.now().difference(r.timerEndTime).inSeconds, closeTo(1, 1));
+      expect(rm.allReminders, contains(r));
     });
     test('reminder for deadline', () {
       final ReminderManager rm = ReminderManager();
-      rm.createReminderForDeadline(DateTime.now().add(Duration(seconds: 1)));
+      final r = rm.createReminderForDeadline(DateTime.now().add(Duration(seconds: 1)));
+      expect(rm.allReminders, contains(r));
     });
-    test('register task with reminder', () {});
+    test('register task with reminder', () {
+      final ReminderManager rm = ReminderManager();
+      final reminder = rm.createReminderForTimer(Duration(seconds: 1));
+      final Task task = Task(taskName: "foo");
+      
+      expect(rm.taskReminderMap, isEmpty);
+      rm.registerTaskWithReminder(reminder, task);
+      expect(rm.taskReminderMap, isNotEmpty);
+      expect(rm.taskReminderMap, containsPair(reminder, task));
+    });
+    test('register duplicate task with reminder', () {
+      final ReminderManager rm = ReminderManager();
+      final reminder = rm.createReminderForTimer(Duration(seconds: 1));
+      final Task task = Task(taskName: "foo");
+      
+      rm.registerTaskWithReminder(reminder, task);
+      rm.registerTaskWithReminder(reminder, task);
+      expect(rm.taskReminderMap, containsPair(reminder, task));
+      expect(rm.taskReminderMap, hasLength(1));
+      expect(rm.taskReminderMap[Task(taskName: "bar")], null);
+    });
+    test('register same task with multiple reminders', () {
+      // we want this to be possible. Suppose a 5-minute-to-end reminder and end reminder
+      final ReminderManager rm = ReminderManager();
+      final reminder1 = rm.createReminderForTimer(Duration(seconds: 1));
+      final reminder2 = rm.createReminderForTimer(Duration(seconds: 1));
+      final Task task = Task(taskName: "foo");
+      
+      rm.registerTaskWithReminder(reminder1, task);
+      rm.registerTaskWithReminder(reminder2, task);
+      expect(rm.taskReminderMap, containsPair(reminder1, task));
+      expect(rm.taskReminderMap, containsPair(reminder2, task));
+      expect(rm.taskReminderMap, hasLength(2));
+    });
+    test('unregister reminder', () {
+      final ReminderManager rm = ReminderManager();
+      final reminder1 = rm.createReminderForTimer(Duration(seconds: 1));
+      final reminder2 = rm.createReminderForTimer(Duration(seconds: 1));
+      final reminder3 = rm.createReminderForTimer(Duration(seconds: 1));
+      final Task task1 = Task(taskName: "foo");
+      final Task task2 = Task(taskName: "bar");
+      rm.registerTaskWithReminder(reminder1, task1);
+      rm.registerTaskWithReminder(reminder2, task2);
+      rm.registerTaskWithReminder(reminder3, task2);
+
+      rm.unregisterReminder(reminder1);
+      expect(rm.taskReminderMap, isNot(containsPair(reminder1, task1)));
+
+      rm.unregisterReminder(reminder2);
+      // task2 still has reminder3
+      expect(rm.taskReminderMap, containsPair(reminder3, task2));
+    });
+    test('unregister nonexistent reminder', () {
+      final ReminderManager rm = ReminderManager();
+      final reminder1 = rm.createReminderForTimer(Duration(seconds: 1));
+
+      expect(rm.taskReminderMap, isEmpty);
+      rm.unregisterReminder(reminder1);
+      expect(rm.taskReminderMap, isEmpty);
+    });
+    test('unregister all reminders of a task', () {
+      final ReminderManager rm = ReminderManager();
+      final reminder1 = rm.createReminderForTimer(Duration(seconds: 1));
+      final reminder2 = rm.createReminderForTimer(Duration(seconds: 1));
+      final reminder3 = rm.createReminderForTimer(Duration(seconds: 1));
+      final task1 = Task(taskName: "foo");
+      final task2 = Task(taskName: "bar");
+
+      rm.registerTaskWithReminder(reminder1, task1);
+      rm.registerTaskWithReminder(reminder3, task1);
+      rm.registerTaskWithReminder(reminder2, task2);
+      expect(rm.taskReminderMap.length, 3);
+
+      rm.unregisterAllRemindersOfTask(task1);
+      expect(rm.taskReminderMap, containsPair(reminder2, task2));
+      expect(rm.taskReminderMap, isNot(containsPair(reminder1, task1)));
+      expect(rm.taskReminderMap, isNot(containsPair(reminder3, task1)));
+
+      rm.unregisterAllRemindersOfTask(task2);
+      expect(rm.taskReminderMap, isEmpty);
+    });
+    test('unregister all reminders of a task', () {
+      final ReminderManager rm = ReminderManager();
+      final Task task = Task(taskName: "foo");
+
+      rm.unregisterAllRemindersOfTask(task);
+      expect(rm.taskReminderMap, isEmpty);
+    });
+    test('get all reminders of task', () {
+      final ReminderManager rm = ReminderManager();
+      final Task task = Task(taskName: "foo");
+      final reminder1 = rm.createReminderForTimer(Duration(seconds: 1));
+      final reminder2 = rm.createReminderForTimer(Duration(seconds: 1));
+      final reminder3 = rm.createReminderForTimer(Duration(seconds: 1));
+      rm.registerTaskWithReminder(reminder1, task);
+      rm.registerTaskWithReminder(reminder3, task);
+
+      List<Reminder> list = rm.getAllRemindersOfTask(task);
+      expect(list, <Reminder>[reminder1, reminder3]);
+    });
+    test('reminder manager callbacks', () => null, skip: "TODO reminder manager callbacks");
   });
   group('test Reminder object', () {
-    test('Constructor', () {
-      final Reminder a = Reminder(DateTime.now(), DateTime.now());
+    dummyCallback(){};
+    test('Constructor', () async {
+      final Reminder a = Reminder(DateTime.now(), DateTime.now().add(Duration(milliseconds: 1)), dummyCallback);
+      expect(a.timer.isActive, isTrue);
+      await(Future.delayed(Duration(milliseconds: 2)));
+    });
+    test('reminder total duration', () async {
+      final Reminder a = Reminder(DateTime.now(), DateTime.now().add(Duration(milliseconds: 1)), dummyCallback);
+      expect(a.totalDuration.inMilliseconds, 1);
+      await(Future.delayed(Duration(milliseconds: 2)));
+    });
+    test('reminder remaining duration', () async {
+      final Reminder a = Reminder(DateTime.now(), DateTime.now().add(Duration(milliseconds: 1)), dummyCallback);
+      expect(a.remainingDuration, lessThan(a.totalDuration));
+      await(Future.delayed(Duration(milliseconds: 2)));
+    });
+    test('reminder callback', () async {
+      bool fired = false;
+      final Reminder a = Reminder(DateTime.now(), DateTime.now().add(Duration(milliseconds: 1)), () => fired = true);
+      await(Future.delayed(Duration(milliseconds: 2)));
+      expect(fired, isTrue);
     });
   });
 }
