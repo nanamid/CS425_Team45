e8c394f9cfa8489028a70b3e6e5b572b88d5c2c7
nanamid <nanamid@nevada.unr.edu>
2024-04-11 13:27:09 -0700

    Task List Retrieval w/ Firebase
    I revamped the functions in "firestore.dart" (which will eventually replace the giant chunk of function in "tasks_page.dart") for a more streamlined look and added the get tasks function. I also started a new task page to implement the new Firebase functions in. The hope is that if this new page works, I can transfer the code into the current task page file (basically cutting out the Hive portion and using only Firebase).

---
+++ Basic_Flutter_App/flutter_walkthrough_app/lib/pages/tasks_page_DB.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TaskPageDB extends StatefulWidget {
+  const TaskPageDB({super.key});
+
+  @override
+  State<TaskPageDB> createState() => _TaskPageDBState();
+}
+
+class _TaskPageDBState extends State<TaskPageDB> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- Basic_Flutter_App/flutter_walkthrough_app/lib/utils/firestore.dart
+++ Basic_Flutter_App/flutter_walkthrough_app/lib/utils/firestore.dart
@@ -1,62 +1,52 @@
+//Inspiration: https://www.youtube.com/watch?v=iQOvD0y-xnw
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class FirestoreService {
+  // Variables
+  final CollectionReference userTasks = FirebaseFirestore.instance
+      .collection("users")
+      .doc(FirebaseAuth.instance.currentUser?.email)
+      .collection("tasks");
+
   // Function for adding a new task
-  // The completion field defaults to "false"
-  addTask_v2(String newTaskName, String newTaskDesc) async {
+  // The completion field defaults to "false" (a new task isn't completed)
+  Future<void> addTask_v2(String newTaskName, String newTaskDesc) async {
     final newTask = {
       "completed": false,
       "taskName": newTaskName,
       "taskDesc": newTaskDesc
     };
-    FirebaseFirestore.instance
-        .collection("users")
-        .doc(FirebaseAuth.instance.currentUser?.email)
-        .collection("tasks")
+    userTasks
         .add(newTask)
         .then((value) =>
             print("Note Added to ${FirebaseAuth.instance.currentUser?.email}"))
         .catchError((error) => print("Failed to add note: $error"));
   }
 
+  // Function for retrieving the task list from the database
+  Stream<QuerySnapshot> getTasksStream() {
+    final tasksStream =
+        userTasks.orderBy("completed", descending: true).snapshots();
+    return tasksStream;
+  }
+
   // Function for updating a task
   // This function is only for updating the text of the task
-  updateTask(String newTaskName, String newTaskDesc) async {
+  Future<void> updateTask(
+      String docID, String newTaskName, String newTaskDesc) async {
     final updateTask = {
       "completed": false,
       "taskName": newTaskName,
       "taskDesc": newTaskDesc
     };
-    var docID = FirebaseFirestore.instance
-        .collection("users")
-        .doc(FirebaseAuth.instance.currentUser?.email)
-        .collection("tasks")
-        .doc()
-        .id;
-    FirebaseFirestore.instance
-        .collection("users")
-        .doc(FirebaseAuth.instance.currentUser?.email)
-        .collection("tasks")
-        .doc(docID)
-        .update(updateTask)
-        .then((value) => print(
-            "Note Updated from ${FirebaseAuth.instance.currentUser?.email}"))
-        .catchError((error) => print("Failed to update note: $error"));
+    return userTasks.doc(docID).update(updateTask);
   }
 
   // Function for marking if a task is done
-  isDone(bool isTaskDone) async {
-    var docID = FirebaseFirestore.instance
-        .collection("users")
-        .doc(FirebaseAuth.instance.currentUser?.email)
-        .collection("tasks")
-        .doc()
-        .id;
-    FirebaseFirestore.instance
-        .collection("users")
-        .doc(FirebaseAuth.instance.currentUser?.email)
-        .collection("tasks")
+  Future<void> isDone(String docID, bool isTaskDone) async {
+    userTasks
         .doc(docID)
         .update({"completed": isTaskDone})
         .then((value) => print(
@@ -65,17 +55,8 @@ class FirestoreService {
   }
 
   // Function for deleting a task
-  deleteTask() async {
-    var docID = FirebaseFirestore.instance
-        .collection("users")
-        .doc(FirebaseAuth.instance.currentUser?.email)
-        .collection("tasks")
-        .doc()
-        .id;
-    FirebaseFirestore.instance
-        .collection("users")
-        .doc(FirebaseAuth.instance.currentUser?.email)
-        .collection("tasks")
+  Future<void> deleteTask(String docID) async {
+    userTasks
         .doc(docID)
         .delete()
         .then((value) => print(
