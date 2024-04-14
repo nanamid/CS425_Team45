e065c47d4673a77e0d6f271301614f17ce609f03
wpunr <williampeterson@unr.edu>
2024-04-13 20:16:32 -0700

    Separate task_add_page into task_modify_widget

--- Basic_Flutter_App/flutter_walkthrough_app/lib/features/task/screens/task_add_page.dart
+++ Basic_Flutter_App/flutter_walkthrough_app/lib/features/task/screens/task_add_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:test_app/common/widgets/build_text.dart';
 import 'package:test_app/features/task/screens/confirm_addtask_dialogs.dart';
+import 'package:test_app/features/task/screens/task_modify_widget.dart';
 import 'package:test_app/navigation_menu.dart';
 import 'package:test_app/utils/constants/colors.dart';
 import 'package:test_app/utils/constants/image_strings.dart';
@@ -11,207 +12,42 @@ import 'package:test_app/data/tasklist_classes.dart';
 import 'package:get/get.dart';
 
 // Might be better to do this in a form widget
-class AddTaskPage extends StatefulWidget {
+class AddTaskPage extends StatelessWidget {
   const AddTaskPage({super.key});
 
   @override
-  State<AddTaskPage> createState() => _AddTaskPageState();
-}
+  Widget build(BuildContext context) {
+    final NavigationController controller = Get.find();
+    final Task newTask = Task(taskName: "");
 
-class _AddTaskPageState extends State<AddTaskPage> {
-  final NavigationController controller = Get.find();
+    void saveTask() {
+      TodoDatabase db = TodoDatabase();
+      final taskListIndex = 0; // hardcoded one tasklist for now
+      db.loadData();
+      TaskList taskList = db.listOfTaskLists[taskListIndex];
+      taskList.addTask(newTask);
 
-  final TextEditingController taskNameController = TextEditingController();
-  // final TextEditingController taskStatusController = TextEditingController();
-  TaskStatus taskStatus = TaskStatus.TODO;
-  // final TextEditingController taskLabelController = TextEditingController();
-  TaskLabel taskLabel = TaskLabel.Default;
-  final TextEditingController taskDescriptionController =
-      TextEditingController();
-  DateTime? deadline;
-  // TODO parent task
-
-  void _saveTask() {
-    TodoDatabase db = TodoDatabase();
-    final taskListIndex = 0; // hardcoded one tasklist for now
-    db.loadData();
-
-    TaskList taskList = db.listOfTaskLists[taskListIndex];
-    Task newTask = Task(
-      taskName: taskNameController.text,
-      taskStatus: taskStatus,
-      taskLabel: taskLabel,
-      taskDescription: taskDescriptionController.text,
-      taskDeadline: deadline,
-    );
-    taskList.addTask(newTask);
-    taskNameController.clear();
-    taskDescriptionController.clear();
-
-    if (deadline != null) {
-      db.reminderManager.registerTaskWithReminder(
-          db.reminderManager.createReminderForDeadline(deadline!), newTask);
+      if (newTask.taskDeadline != null) {
+        db.reminderManager.registerTaskWithReminder(
+            db.reminderManager.createReminderForDeadline(newTask.taskDeadline!),
+            newTask);
+      }
+      db.updateDatabase();
+      controller.selectedIndex.value = 0; // hardcoded task list page index
     }
 
-    db.updateDatabase();
+    void cancelSaveTask() {
+      controller.selectedIndex.value = 0; // hardcoded task list page index
+    }
 
-    controller.selectedIndex.value = 0; // hardcoded task list page index
-  }
-
-  void _cancelSaveTask() {
-    controller.selectedIndex.value = 0; // hardcoded task list page index
-  }
-
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: AppColors.accent,
         body: SafeArea(
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-              // Name
-              TextField(
-                controller: taskNameController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  fillColor: AppColors.primary,
-                  filled: true,
-                  hintText: "Task Name",
-                ),
-              ),
-
-              // Status
-              DropdownMenu(
-                initialSelection: TaskStatus.TODO,
-                helperText: 'Task Status',
-                inputDecorationTheme: InputDecorationTheme(
-                    fillColor: AppColors.primary, filled: true),
-                onSelected: (TaskStatus? status) {
-                  if (status != null) {
-                    setState(() => taskStatus = status);
-                  }
-                },
-                dropdownMenuEntries: TaskStatus.values
-                    .map<DropdownMenuEntry<TaskStatus>>((TaskStatus status) {
-                  return DropdownMenuEntry<TaskStatus>(
-                    value: status,
-                    label: status.label,
-                  );
-                }).toList(),
-              ),
-
-              // Label
-              DropdownMenu(
-                initialSelection: TaskLabel.Default,
-                helperText: 'Task Label',
-                inputDecorationTheme: InputDecorationTheme(
-                    fillColor: AppColors.primary, filled: true),
-                onSelected: (TaskLabel? label) {
-                  if (label != null) {
-                    setState(() => taskLabel = label);
-                  }
-                },
-                dropdownMenuEntries: TaskLabel.values
-                    .map<DropdownMenuEntry<TaskLabel>>(
-                        (TaskLabel dropdownLabel) {
-                  return DropdownMenuEntry<TaskLabel>(
-                    value: dropdownLabel,
-                    label: dropdownLabel.label,
-                  );
-                }).toList(),
-              ),
-
-              // Description
-              TextField(
-                controller: taskDescriptionController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  fillColor: AppColors.primary,
-                  filled: true,
-                  hintText: "Description",
-                ),
-              ),
-
-              ElevatedButton(
-                onPressed: () async {
-                  deadline = await showDatePicker(
-                      context: context,
-                      firstDate: DateTime.now(),
-                      lastDate: DateTime.utc(9999, 01, 01));
-                  setState(() {});
-                },
-                child: Text(deadline == null
-                    ? "Deadline Date"
-                    : "${deadline!.year}-${deadline!.month}-${deadline!.day}"),
-              ),
-
-              ElevatedButton(
-                  onPressed: () async {
-                    if (deadline != null) // date has to be entered first
-                    {
-                      TimeOfDay? tempTime = await showTimePicker(
-                          context: context,
-                          initialTime: TimeOfDay(hour: 0, minute: 0));
-                      if (tempTime != null) {
-                        setState(() {
-                          deadline = deadline!.add(Duration(
-                              hours: tempTime.hour, minutes: tempTime.minute));
-                        });
-                      }
-                    }
-                  },
-                  child: Text(deadline == null
-                      ? "Time"
-                      : "${deadline!.hour.toString().padLeft(2, '0')}:${deadline!.minute.toString().padLeft(2, '0')}")),
-
-              //Button to save or cancel input
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  //SAVE
-                  ElevatedButton(
-                    style: TextButton.styleFrom(
-                        backgroundColor: AppColors.buttonPrimary),
-                    onPressed: () async {
-                      bool? confirmation;
-                      confirmation = await confirmSaveTaskDialog(context);
-                      if (confirmation == true) {
-                        _saveTask();
-                      }
-                    },
-                    child: buildText(
-                        'Save',
-                        AppColors.textWhite,
-                        AppSizes.textSmall,
-                        FontWeight.normal,
-                        TextAlign.left,
-                        TextOverflow.clip),
-                  ),
-
-                  const SizedBox(width: 8),
-
-                  //CANCEL
-                  ElevatedButton(
-                    style: TextButton.styleFrom(
-                        backgroundColor: AppColors.buttonSecondary),
-                    onPressed: () async {
-                      bool? confirmation;
-                      confirmation = await confirmCancelTaskDialog(context);
-                      if (confirmation == true) {
-                        _cancelSaveTask();
-                      }
-                    },
-                    child: buildText(
-                        'Cancel',
-                        AppColors.textWhite,
-                        AppSizes.textSmall,
-                        FontWeight.normal,
-                        TextAlign.left,
-                        TextOverflow.clip),
-                  ),
-                ],
-              )
-            ])));
+          child: ModifyTaskWidget(
+            saveTask: saveTask,
+            cancelSaveTask: cancelSaveTask,
+            task: newTask,
+          ),
+        ));
   }
 }
